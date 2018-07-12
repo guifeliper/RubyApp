87362bc26dc82e1c5271556cfbace82f1316c214
guifeliper <guifeliper@gmail.com>
2018-07-12 21:53:18 +0000

    Implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,8 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
+    accepts_nested_attributes_for :technologies, 
+                                   reject_if: lambda { |attrs| attrs['name'].blank?}
+
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     def self.angular
@@ -12,6 +15,6 @@ class Portfolio < ApplicationRecord
 
     def set_defaults
         self.main_image ||= "http://placehold.it/600x400"
-        self.main_image ||= "http://placehold.it/350x200"
+        self.thumb_image ||= "http://placehold.it/350x200"
     end
 end
